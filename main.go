b47ea8ebcec728a9874c16508a10ceca95b380d3
wayand bahramzy <lawangjan@hotmail.com>
2024-09-21 19:50:49 +0200

    commit using deploy sh script

--- main.go
+++ main.go
@@ -13,11 +13,48 @@ type Payload struct {
 	Ref string `json:"ref"`
 }
 
+// Function to handle the deployment in the background
+func deploy(project string, projectPath string) {
+
+	// Mark the project directory as safe for Git operations
+	// exec.Command("git", "config", "--global", "--add", "safe.directory", projectPath).Run()
+
+	token := os.Getenv("GITHUB_TOKEN")
+	user := os.Getenv("GITHUB_USER")
+	if token == "" || user == "" {
+		fmt.Println("token or user is not set")
+		return
+	}
+
+	// GitHub repo URL using HTTPS and the token for authentication
+	repoURL := fmt.Sprintf("https://%s@github.com/%s/%s.git", token, user, project)
+
+	// Command to pull the latest code and redeploy using Docker Compose
+	cmdText := fmt.Sprintf(`
+                cd %s &&
+				git remote set-url origin %s &&
+                git fetch --depth=1 origin master &&
+				git reset --hard FETCH_HEAD &&
+                docker-compose down &&
+                docker-compose up -d --build
+            `, projectPath, repoURL)
+	cmd := exec.Command("bash", "-c", cmdText)
+
+	// Execute the command
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error deploying %s: %v\n%s", project, err, string(output)))
+		fmt.Println("we try to run this command: ", cmdText)
+	} else {
+		fmt.Printf("Deployment for %s successful\n%s", project, string(output))
+	}
+}
+
 func deployHandler(project string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var payload Payload
 		if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
-			fmt.Printf("Bad request for project %s: %v", project, err)
+			fmt.Println(fmt.Sprintf("Bad request for project %s: %v", project, err))
 			http.Error(w, "Bad Request", http.StatusBadRequest)
 			return
 		}
@@ -34,43 +71,12 @@ func deployHandler(project string) http.HandlerFunc {
 			// Define the folder based on the project and the environment variable
 			projectPath := fmt.Sprintf("%s/%s", projectsFolder, project)
 
-			token := os.Getenv("GITHUB_TOKEN")
-			user := os.Getenv("GITHUB_USER")
-			if token == "" || user == "" {
-				fmt.Println("token or user is not set")
-				http.Error(w, "token or user is not set", http.StatusInternalServerError)
-				return
-			}
+			// Respond immediately to the webhook request to avoid GitHub timeout
+			fmt.Fprintf(w, "Webhook received. Deployment started for %s", project)
 
-			// Mark the project directory as safe for Git operations
-			exec.Command("git", "config", "--global", "--add", "safe.directory", projectPath).Run()
+			// Run the deployment process in the background
+			go deploy(project, projectPath)
 
-			// GitHub repo URL using HTTPS and the token for authentication
-			repoURL := fmt.Sprintf("https://%s@github.com/%s/%s.git", token, user, project)
-
-			// Command to pull the latest code and redeploy using Docker Compose or any other method
-			cmdText := fmt.Sprintf(`
-                cd %s &&
-				git remote set-url origin %s &&
-				git clean -fd &&
-                git fetch --depth=1 origin master &&
-				git reset --hard FETCH_HEAD &&
-                docker-compose down &&
-                docker-compose up -d --build
-            `, projectPath, repoURL)
-			cmd := exec.Command("bash", "-c", cmdText)
-
-			// Execute the deployment scripts
-			output, err := cmd.CombinedOutput()
-			if err != nil {
-				fmt.Println(fmt.Sprintf("Error deploying %s: %v\n%s", project, err, string(output)))
-				fmt.Println("we try to run this command: ", cmdText)
-				http.Error(w, fmt.Sprintf("Error deploying %s: %v\n%s", project, err, string(output)), http.StatusInternalServerError)
-				return
-			}
-
-			fmt.Printf("Deployment for %s successful", project)
-			fmt.Fprintf(w, "Deployment for %s successful", project)
 		} else {
 			fmt.Println("Not a master branch push")
 			fmt.Fprintf(w, "Not a master branch push")
